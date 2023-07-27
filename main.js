68a000a1356cff1318f71ca6813a2e12ac55d290
Shaikhul Islam <shaikhul@github.com>
2023-07-27 12:24:40 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log("Hello, World");
+
+// copied from one of Ana's repo :)
+
+document.write(window.location.search);
+/*
+This ^^ causes an alert that won't be reported in the PR because
+it was already in the code.
+*/
+
+// 2
+require('crypto').createCipheriv('aes-256-cfb', '0123456789bbbbbb0123456789bbbbbb', '0123456789bbbbbb');
